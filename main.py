ee16fc927010924f394246d0d72629516fe181b6
rohit-singh-git <smartyonpc@gmail.com>
2024-05-22 13:29:36 +0530

    Predictor Complete Code

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Form, Request, Query, HTTPException
 from fastapi.responses import HTMLResponse, JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+
 import model
 
 app = FastAPI()
@@ -13,12 +14,6 @@ app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
 
-@app.on_event("startup")
-async def startup_event():
-    await model.cache_stock_info()
-
-
-# Define routes
 @app.get("/", response_class=HTMLResponse)
 async def get_basic_form(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
@@ -29,13 +24,14 @@ async def submit_form(request: Request, open: float = Form(...), high: float = F
                       nse_symbol: str = Form(...)):
     try:
         stock_symbol = None
-        for stock in model.stock_info_cache:
-            if nse_symbol.lower() in stock["symbol"].lower() or nse_symbol.lower() in stock["name"].lower():
-                stock_symbol = stock["symbol"]
+        for stock in model.stock_list:
+            if nse_symbol.lower() in stock["SYMBOL"].lower() or nse_symbol.lower() in stock["NAMEOFCOMPANY"].lower():
+                stock_symbol = stock["SYMBOL"]
                 break
         if not stock_symbol:
             raise ValueError("Stock not found.")
-        await model.train_model(nse_symbol)
+
+        model.train_model(stock_symbol)
         pred = model.predict(open, high, low)
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
@@ -44,6 +40,12 @@ async def submit_form(request: Request, open: float = Form(...), high: float = F
 
 @app.get("/autocomplete")
 async def autocomplete_stock_names(query: str = Query(..., min_length=1)):
-    matched_names = [stock for stock in model.stock_info_cache if
-                     query.lower() in stock["symbol"].lower() or query.lower() in stock["name"].lower()]
+    matched_names = [stock for stock in model.stock_list if
+                     query.lower() in stock["SYMBOL"].lower() or query.lower() in stock["NAMEOFCOMPANY"].lower()]
     return JSONResponse(content=matched_names)
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
--- model.py
+++ model.py
@@ -1,34 +1,20 @@
+import pandas as pd
 import yfinance as yf
 from sklearn.exceptions import NotFittedError
-from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
 from sklearn.tree import DecisionTreeRegressor
-import pandas as pd
-import asyncio
 
 lr_model = LinearRegression()
 dtr_model = DecisionTreeRegressor()
 
 data = pd.read_csv("Stock_names.csv")
-symbols = data["SYMBOL"].tolist()
-stock_info_cache = []
+stock_list = data.to_dict(orient="records")
 
 
-async def fetch_stock_info(symbol):
-    ticker = yf.Ticker(f"{symbol}.ns")
-    info = ticker.info
-    return {
-        "symbol": symbol,
-        "name": info.get("longName", "")
-    }
 
 
-async def cache_stock_info():
-    global stock_info_cache
-    stock_info_cache = await asyncio.gather(*(fetch_stock_info(symbol) for symbol in symbols))
-
-
-async def train_model(stock_name):
+def train_model(stock_name):
     data = yf.Ticker(f'{stock_name}.ns')
 
     hist = data.history("max")
@@ -38,7 +24,7 @@ async def train_model(stock_name):
     X = hist[["Open", "High", "Low"]]
     y = hist["Close"]
 
-    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, train_size=0.8, random_state=1)
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=1)
 
     lr_model.fit(X_train, y_train)
     dtr_model.fit(X_train, y_train)
@@ -54,12 +40,3 @@ def predict(open, high, low):
 
     return lr[0], dtr[0]
 
-# def get_stock_list():
-#     for symbol in symbols:
-#         ticker = yf.Ticker(f"{symbol}.ns")
-#         info = ticker.info
-#         stock_info.append({
-#             "symbol": symbol,
-#             "name": info.get("longName", "")
-#         })
-#     return stock_info
